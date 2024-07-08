91254fb475fbf17f3333fa85f9afc9af97b883cd
Michal Sek <michal.sek@swmansion.com>
2024-07-08 20:19:57 +0200

    feat: jsi my ios'ai

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,36 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 import { multiply } from 'react-native-another-test';
 
 export default function App() {
+  const [numA, setNumA] = useState<number>(0);
+  const [numB, setNumB] = useState<number>(0);
   const [result, setResult] = useState<number | undefined>();
 
   useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+    setResult(multiply(numA, numB));
+  }, [numA, numB]);
 
   return (
     <View style={styles.container}>
+      <View>
+        <View style={styles.row}>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Number A"
+            onChangeText={(text) => setNumA(parseInt(text, 10))}
+            style={styles.textInput}
+          />
+        </View>
+        <View style={styles.row}>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Number B"
+            onChangeText={(text) => setNumB(parseInt(text, 10))}
+            style={styles.textInput}
+          />
+        </View>
+      </View>
       <Text>Result: {result}</Text>
     </View>
   );
@@ -21,10 +41,24 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 24,
   },
   box: {
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    width: '100%',
+    marginBottom: 16,
+  },
+  textInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'black',
+    height: 40,
+    width: '100%',
+    padding: 12,
   },
 });
